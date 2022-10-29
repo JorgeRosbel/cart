94c67cb224ba8ba92a977318c2746cc429628d11
JorgeRosbel <94653105+JorgeRosbel@users.noreply.github.com>
2022-10-29 09:57:36 -0700

    Add files via upload

--- js/allergens.js
+++ js/allergens.js
@@ -17,51 +17,58 @@ function setAllergens(elements,id){
 
 var producto_1 = new Array();
 producto_1[0] = ['alergenos/camaron.png'];
-producto_1[1] = ['alergenos/camaron.png'];
+producto_1[1] = ['alergenos/cangrejo.png'];
+producto_1[2] = ['alergenos/mani.png'];
 
 var producto_2 = new Array();
-producto_2[0] = ['alergenos/camaron.png'];
-producto_2[1] = ['alergenos/camaron.png'];
+producto_2[0] = ['alergenos/nuez.png'];
+producto_2[1] = ['alergenos/leche.png'];
 
 var producto_3 = new Array();
-producto_3[0] = ['alergenos/camaron.png'];
-producto_3[1] = ['alergenos/camaron.png'];
+producto_3[0] = ['alergenos/sal.png'];
+producto_3[1] = ['alergenos/trigo.png'];
+producto_3[2] = ['alergenos/leche.png'];
 
 var producto_4 = new Array();
-producto_4[0] = ['alergenos/camaron.png'];
-producto_4[1] = ['alergenos/camaron.png'];
+producto_4[0] = ['alergenos/leche.png'];
+
 
 var producto_5 = new Array();
-producto_5[0] = ['alergenos/camaron.png'];
-producto_5[1] = ['alergenos/camaron.png'];
+producto_5[0] = ['alergenos/leche.png'];
 
 var producto_6 = new Array();
-producto_6[0] = ['alergenos/camaron.png'];
-producto_6[1] = ['alergenos/camaron.png'];
+producto_6[0] = ['alergenos/leche.png'];;
 
 var producto_7 = new Array();
-producto_7[0] = ['alergenos/camaron.png'];
-producto_7[1] = ['alergenos/camaron.png'];
+producto_7[0] = ['alergenos/sal.png'];
+producto_7[1] = ['alergenos/trigo.png'];
+producto_7[2] = ['alergenos/huevo.png'];
+producto_7[3] = ['alergenos/chorizo.png'];
+
 
 var producto_8 = new Array();
-producto_8[0] = ['alergenos/camaron.png'];
-producto_8[1] = ['alergenos/camaron.png'];
+producto_8[0] = ['alergenos/sal.png'];
+producto_8[1] = ['alergenos/huevo.png'];
 
 var producto_9 = new Array();
-producto_9[0] = ['alergenos/camaron.png'];
-producto_9[1] = ['alergenos/camaron.png'];
+producto_9[0] = ['alergenos/pez.png'];
+producto_9[1] = ['alergenos/trigo.png'];
+producto_9[2] = ['alergenos/huevo.png'];
 
 var producto_10 = new Array();
-producto_10[0] = ['alergenos/camaron.png'];
-producto_10[1] = ['alergenos/camaron.png'];
+producto_10[0] = ['alergenos/huevo.png'];
+producto_10[1] = ['alergenos/leche.png'];
+producto_10[2] = ['alergenos/trigo.png'];
 
 var producto_11 = new Array();
-producto_11[0] = ['alergenos/camaron.png'];
-producto_11[1] = ['alergenos/camaron.png'];
+producto_11[0] = ['alergenos/huevo.png'];
+producto_11[1] = ['alergenos/leche.png'];
+producto_11[2] = ['alergenos/trigo.png'];
 
 var producto_12 = new Array();
-producto_12[0] = ['alergenos/camaron.png'];
-producto_12[1] = ['alergenos/camaron.png'];
+producto_12[0] = ['alergenos/huevo.png'];
+producto_12[1] = ['alergenos/leche.png'];
+producto_12[2] = ['alergenos/trigo.png'];
 
 
 //************************************************************************************************************************************************
